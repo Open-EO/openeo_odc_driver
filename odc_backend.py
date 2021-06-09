01ac1cff9edc65a9ee0045cec8482e0128f45aff
clausmichele <claus.michele@hotmail.it>
2021-06-09 15:45:04 +0200

    Updated metadata endpoint

--- odc_backend.py
+++ odc_backend.py
@@ -17,7 +17,9 @@ import datacube
 DATACUBE_EXPLORER_ENDPOINT = "http://0.0.0.0:9000"
 OPENDATACUBE_CONFIG_FILE = ""
 USE_CACHED_COLLECTIONS = True
-ODC_COLLECTIONS_FILE = "ODC_collections.json"
+## TODO: add check if metadata folder exists, otherwise create it
+METADATA_FOLDER = "./DATACUBES/METADATA/"
+ODC_COLLECTIONS_FILE = METADATA_FOLDER + "/CACHE/" + "ODC_collections.json"
 
 def sar2cube_collection_extent(collectionName):
     dc = datacube.Datacube(config = OPENDATACUBE_CONFIG_FILE)
@@ -55,30 +57,17 @@ def list_collections():
                 return jsonify(stacCollection)
         except IOError:
             pass
-            
     res = requests.get(DATACUBE_EXPLORER_ENDPOINT + "/products.txt")
     datacubesList = res.text.split('\n')
     collections = {}
-    collections['collections'] = {}
+    collections['collections'] = []
+    collectionsList = []
     for i,d in enumerate(datacubesList):
-        ## TODO: if SAR2Cube datacube, we need to read the coverage from somewhere else
-        res = requests.get(DATACUBE_EXPLORER_ENDPOINT + "/collections/" + d)
-        stacCollection = res.json()
-        stacCollection.pop('properties')
-        stacCollection['license'] = 'CC-BY-4.0'
-        stacCollection['providers'] = [{'name': 'Eurac EO ODC', 'url': 'http://www.eurac.edu/', 'roles': ['producer','host']}]
-        stacCollection['links'] = {}
-        stacCollection['links'] = [{'rel' : 'license', 'href' : 'https://creativecommons.org/licenses/by/4.0/', 'type' : 'text/html', 'title' : 'License link'}]
-        collections['collections'][i] = stacCollection
-        if "SAR2Cube" in d:
-            try:
-                sar2cubeBbox = sar2cube_collection_extent(d)
-                stacCollection['extent']['spatial']['bbox'] = [sar2cubeBbox]
-            except:
-                pass
-    with open('ODC_collections.json', 'w') as outfile:
+        currentCollection = construct_stac_collection(d)
+        collectionsList.append(currentCollection)
+    collections['collections'] = collectionsList
+    with open(ODC_COLLECTIONS_FILE, 'w') as outfile:
         json.dump(collections, outfile)
-        
     return jsonify(collections)
 
 
@@ -86,96 +75,132 @@ def list_collections():
 def describe_collection(name):
     if USE_CACHED_COLLECTIONS:
         try:
-            f = open(name + '.json')
+            f = open(METADATA_FOLDER + '/CACHE/' + name + '.json')
             # Do something with the file
-            with open(name + '.json') as collection:
+            with open(METADATA_FOLDER + '/CACHE/' + name + '.json') as collection:
                 stacCollection = json.load(collection)
                 return jsonify(stacCollection)
-        except IOError:
+        except Exception as e:
             pass
+    
+    stacCollection = construct_stac_collection(name)
         
-    res = requests.get(DATACUBE_EXPLORER_ENDPOINT + "/collections/" + name)
+    return jsonify(stacCollection)
+
+def construct_stac_collection(collectionName):
+    res = requests.get(DATACUBE_EXPLORER_ENDPOINT + "/collections/" + collectionName)
     stacCollection = res.json()
+    metadata = None
+    try:
+        additional_metadata = open(METADATA_FOLDER + '/SUPP/' + collectionName + '_supp_metadata.json')
+        metadata = json.load(additional_metadata)
+    except Exception as e:
+        print(e)
+
+    stacCollection['stac_extensions'] = ['datacube']         
     stacCollection.pop('properties')
     stacCollection['license'] = 'CC-BY-4.0'
     stacCollection['providers'] = [{'name': 'Eurac EO ODC', 'url': 'http://www.eurac.edu/', 'roles': ['producer','host']}]
     stacCollection['links'] = {}
     stacCollection['links'] = [{'rel' : 'license', 'href' : 'https://creativecommons.org/licenses/by/4.0/', 'type' : 'text/html', 'title' : 'License link'}]
-    stacCollection['stac_extensions'] = ['datacube']
-    stacCollection['stac_extensions'] = ['datacube']
+    if "SAR2Cube" in collectionName:
+        try:
+            sar2cubeBbox = sar2cube_collection_extent(collectionName)
+            stacCollection['extent']['spatial']['bbox'] = [sar2cubeBbox]
+        except:
+            pass
+
+    ### SUPPLEMENTARY METADATA FROM FILE
+    if metadata is not None:
+        stacCollection['title']       = metadata['title']
+        stacCollection['description'] = metadata['description']
+        if 'keywords' in metadata.keys():
+            stacCollection['keywords']     = metadata['keywords']
+        if 'providers' in metadata.keys():
+            stacCollection['providers']    = metadata['providers']
+        if 'version' in metadata.keys():
+            stacCollection['version']      = metadata['version']
+        if 'deprecated' in metadata.keys():
+            stacCollection['deprecated']   = metadata['deprecated']
+        if 'license' in metadata.keys():
+            stacCollection['license']      = metadata['license']
+        if 'sci:citation' in metadata.keys():
+            stacCollection['sci:citation'] = metadata['sci:citation']
+            stacCollection['stac_extensions'] = ['datacube','scientific']
+        if 'links' in metadata.keys():
+            stacCollection['links']        = metadata['links']
+        if 'summaries' in metadata.keys():
+            stacCollection['summaries'] = {}
+            if 'rows' in metadata['summaries']:
+                stacCollection['summaries']['rows']           = metadata['summaries']['rows']
+            if 'columns' in metadata['summaries']:
+                stacCollection['summaries']['columns']        = metadata['summaries']['columns']
+            if 'gsd' in metadata['summaries']:
+                stacCollection['summaries']['gsd']            = metadata['summaries']['gsd']
+            if 'constellation' in metadata['summaries']:
+                stacCollection['summaries']['constellation']  = metadata['summaries']['constellation']
+            if 'platform' in metadata['summaries']:
+                stacCollection['summaries']['platform']       = metadata['summaries']['platform']
+            if 'instruments' in metadata['summaries']:
+                stacCollection['summaries']['instruments']    = metadata['summaries']['instruments']
+            if 'eo:cloud cover' in metadata['summaries']:
+                stacCollection['summaries']['eo:cloud cover'] = metadata['summaries']['eo:cloud cover']
+
+    ### SPATIAL AND TEMPORAL EXTENT FROM DATACUBE-EXPLORER
     stacCollection['cube:dimensions'] = {}
     stacCollection['cube:dimensions']['DATE'] = {}
     stacCollection['cube:dimensions']['DATE']['type'] = 'temporal'
     stacCollection['cube:dimensions']['DATE']['extent'] = stacCollection['extent']['temporal']['interval'][0]
-    
+
     stacCollection['cube:dimensions']['X'] = {}
     stacCollection['cube:dimensions']['X']['type'] = 'spatial'
     stacCollection['cube:dimensions']['X']['axis'] = 'x'
-    stacCollection['cube:dimensions']['X']['extent'] = [stacCollection['extent']['spatial']['bbox'][0][0],stacCollection['extent']['spatial']['bbox'][0][2]]  
-    stacCollection['cube:dimensions']['X']['reference_system'] = 'unknown'
+    stacCollection['cube:dimensions']['X']['extent'] = [stacCollection['extent']['spatial']['bbox'][0][0],stacCollection['extent']['spatial']['bbox'][0][2]]
 
     stacCollection['cube:dimensions']['Y'] = {}
     stacCollection['cube:dimensions']['Y']['type'] = 'spatial'
     stacCollection['cube:dimensions']['Y']['axis'] = 'y'
     stacCollection['cube:dimensions']['Y']['extent'] = [stacCollection['extent']['spatial']['bbox'][0][1],stacCollection['extent']['spatial']['bbox'][0][3]]
-    stacCollection['cube:dimensions']['Y']['reference_system'] = 'unknown'
-    
-    res = requests.get(DATACUBE_EXPLORER_ENDPOINT + "/collections/" + name + "/items")
+
+    if metadata is not None:
+        if 'crs' in metadata.keys():
+            stacCollection['cube:dimensions']['X']['reference_system'] = metadata['crs']
+            stacCollection['cube:dimensions']['Y']['reference_system'] = metadata['crs']
+
+    res = requests.get(DATACUBE_EXPLORER_ENDPOINT + "/collections/" + collectionName + "/items")
     items = res.json()
+
+    ## TODO: remove this part when all the datacubes have a metadata file, crs comes from there
     yamlFile = items['features'][0]['assets']['location']['href']
-    yamlFile = yamlFile.split('file://')[1].replace('%40','@')
-    
+    yamlFile = yamlFile.split('file://')[1].replace('%40','@').replace('%3A',':')
+
     with open(yamlFile, 'r') as stream:
         try:
             yamlDATA = yaml.safe_load(stream)
             stacCollection['cube:dimensions']['X']['reference_system'] = int(yamlDATA['grid_spatial']['projection']['spatial_reference'].split('EPSG')[-1].split('\"')[-2])
             stacCollection['cube:dimensions']['Y']['reference_system'] = int(yamlDATA['grid_spatial']['projection']['spatial_reference'].split('EPSG')[-1].split('\"')[-2])
-        except yaml.YAMLError as exc:
-            print(exc)
-    
+        except Exception as e:
+            print(e)
+
+    ### BANDS FROM DATACUBE-EXPLORER
     keys = items['features'][0]['assets'].keys()
     list_keys = list(keys)
     list_keys.remove('location')
     bands_list = []
-    for key in list_keys:
-        if len(items['features'][0]['assets'][key]['eo:bands'])>1:
-            for b in items['features'][0]['assets'][key]['eo:bands']:
-                bands_list.append(b)
-        else:
-            bands_list.append(items['features'][0]['assets'][key]['eo:bands'][0])
-            
+    try:
+        for key in list_keys:
+            if len(items['features'][0]['assets'][key]['eo:bands'])>1:
+                for b in items['features'][0]['assets'][key]['eo:bands']:
+                    bands_list.append(b)
+            else:
+                bands_list.append(items['features'][0]['assets'][key]['eo:bands'][0])
+    except Exception as e:
+        print(e)
+
     stacCollection['cube:dimensions']['bands'] = {}
     stacCollection['cube:dimensions']['bands']['type'] = 'bands'
     stacCollection['cube:dimensions']['bands']['values'] = bands_list
-    summaries_dict = {}
-    summaries_dict['constellation'] = ['No Constellation Information Available']
-    summaries_dict['platform']      = ['No Platform Information Available']
-    summaries_dict['instruments']   = ['No Instrument Information Available']
-    cloud_cover_dict = {}
-    cloud_cover_dict['min'] = 0
-    cloud_cover_dict['max'] = 0
-    summaries_dict['eo:cloud cover'] = cloud_cover_dict
-    summaries_dict['eo:gsd'] = [0]
-    eo_bands_list_dict = []
-    for band in bands_list:
-        eo_bands_dict = {}
-        eo_bands_dict['name'] = band
-        eo_bands_dict['common_name'] = band
-        eo_bands_dict['center_wavelength'] = 0
-        eo_bands_dict['gsd'] = 0
-        eo_bands_list_dict.append(eo_bands_dict)
-        
-    summaries_dict['eo:bands'] = eo_bands_list_dict
-    stacCollection['summaries'] = summaries_dict
-    if "SAR2Cube" in name:
-        try:
-            sar2cubeBbox = sar2cube_collection_extent(name)
-            stacCollection['extent']['spatial']['bbox'] = [sar2cubeBbox]
-            stacCollection['cube:dimensions']['X']['extent'] = [sar2cubeBbox[0],sar2cubeBbox[2]]  
-            stacCollection['cube:dimensions']['Y']['extent'] = [sar2cubeBbox[1],sar2cubeBbox[3]]
-        except:
-            pass
-    with open(name + '.json', 'w') as outfile:
+
+    with open(METADATA_FOLDER + '/CACHE/' + collectionName + '.json', 'w') as outfile:
         json.dump(stacCollection, outfile)
-        
-    return jsonify(stacCollection)
+    return stacCollection
