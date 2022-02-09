ab406bc9a6b2ff1108351fe5d2a945bc5b201887
clausmichele <claus.michele@hotmail.it>
2022-02-09 10:50:13 +0100

    Updated error messages

--- openEO_error_messages.py
+++ openEO_error_messages.py
@@ -1,1 +1,11 @@
+#merge_cubes
 OverlapResolverMissing = "Overlapping data cubes, but no overlap resolver has been specified."
+#resample_cube_temporal
+TooManyDimensions = "The number of temporal dimensions must be reduced to one for 'resample_cube_temporal'"
+DimensionNotAvailable = "A dimension with the specified name does not exist."
+#multiply
+MultiplicandMissing = "Multiplication requires at least two numbers."
+#divide
+DivisionByZero = "Division by zero is not supported."
+
+DistinctDimensionLabelsRequired = "The dimension labels have duplicate values. Distinct labels must be specified."
